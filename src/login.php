df5a90e3b04a4231a2518637d5b050ceff1d6e50
Aria-wlj <xhslwlj@gmail.com>
2022-10-13 20:50:48 +1000

    111

---
+++ src/login.php
@@ -0,0 +1,41 @@
+<?php
+    include_once 'header.php';
+    require 'vendor/autoload.php';
+?>
+
+<!-- <?php
+    phpinfo()
+?> -->
+
+
+
+
+
+
+    <div class="page-intro">
+        <div class="top-wrapper"></div>
+        <h1> Log in / Sign up </h1>
+
+    </div>
+
+    <div class="login-form">
+        <form action="signup.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+
+            <button type="submit" name="submit"> Sign Up</button>
+
+        </form>
+
+    <div>
+
+</wrapper>
+  
+
+
+
+
+
+<?php
+ include_once 'footer.php'
+?>
