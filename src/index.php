30f48058064a72f879d0730ca3494935e643cd01
Aria-wlj <xhslwlj@gmail.com>
2022-10-15 16:50:12 +1000

    composer & vendor upload

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<!-- all just a test, remove it when coding this page  -->
+
+
+
+
+<?php
+    phpinfo()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+
+    <p> test PHP in index.html</p>
+    
+</body>
+</html>
+
+
